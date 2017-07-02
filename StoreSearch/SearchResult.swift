4eed643ce9b5a9c8d787de47b95f4dc920159910
CodeMozart <542200245@qq.com>
2017-07-02 17:13:17 +0800

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by officeWanPlus on 2017/7/2.
+//  Copyright © 2017年 Strawberry. All rights reserved.
+//
+
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
